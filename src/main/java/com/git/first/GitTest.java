59fb6273612dc6de9b6737aec73c668884f0ee4d
luoy <2940678188@qq.com>
2022-07-04 21:09:22 +0800

    idea2 commit

--- src/main/java/com/git/first/GitTest.java
+++ src/main/java/com/git/first/GitTest.java
@@ -4,5 +4,6 @@ package com.git.first;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
